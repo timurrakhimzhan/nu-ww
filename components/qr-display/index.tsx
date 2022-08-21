ec5f64ff0132549bdd65c6dc9ba63872ba161b8d
Timur Rakhimzhan <t.rakhimzhan@eyelab.kz>
2022-08-22 02:15:37 +0600

    scan layout fixed

--- components/qr-display/index.tsx
+++ components/qr-display/index.tsx
@@ -3,13 +3,14 @@ import {useSnapshot} from "valtio";
 import moderatorPanelStore from "../../utils/moderator-panel-store";
 import QRCode from "react-qr-code"
 import styles from './QrDisplay.module.scss';
-import {Button, Spinner} from "@chakra-ui/react";
+import {Button, Spinner, useMediaQuery} from "@chakra-ui/react";
 import {trpc} from "../../utils/trpc";
 import {EventParticipationStatus} from "@prisma/client";
 
 const QrDisplay = () => {
     const {hashInfo} = useSnapshot(moderatorPanelStore);
     const [fetching, setFetching] = useState(true);
+    const [isSuperThin] = useMediaQuery('(max-width: 320px)');
     const {data} = trpc.useQuery(['moderator.get-token-moderator-info', { hash: hashInfo?.hash || ''}], {refetchInterval: 5000, enabled: fetching});
     const {mutateAsync: mutateCancel} = trpc.useMutation('moderator.cancel-token');
     const {mutateAsync: mutateAccept} = trpc.useMutation('moderator.accept-token');
@@ -27,7 +28,7 @@ const QrDisplay = () => {
 
     return <div className={styles.wrapper}>
         <div className={styles.qrWrapper}>
-            <QRCode value={`https://hello.world/qr-scanner?hash=${hashInfo.hash}`}/>
+            <QRCode size={isSuperThin ? 196 : 256} value={`https://hello.world/qr-scanner?hash=${hashInfo.hash}`}/>
         </div>
         <div className={styles.info}>
             <div>Status: <b>{data.status}</b></div>
@@ -35,15 +36,14 @@ const QrDisplay = () => {
                 data.scannedUser && (
                     <>
                         <div>ID: <b>{data.scannedUser.id}</b></div>
-                        <div>Email: <b>{data.scannedUser.email}</b></div>
                         <div>Full name: <b>{data.scannedUser.firstName} {data.scannedUser.lastName}</b></div>
                     </>
                 )
             }
         </div>
         <div className={styles.buttonsWrapper}>
-            <Button colorScheme='pink' onClick={() => mutateCancel({hash: hashInfo.hash}).then(() => moderatorPanelStore.reset())}>Cancel</Button>
-            <Button colorScheme='teal' disabled={!data || data.status !== EventParticipationStatus.SCANNED}
+            <Button colorScheme='pink' flex={1} onClick={() => mutateCancel({hash: hashInfo.hash}).then(() => moderatorPanelStore.reset())}>Cancel</Button>
+            <Button colorScheme='teal' flex={1} disabled={!data || data.status !== EventParticipationStatus.SCANNED}
                     onClick={() => mutateAccept({hash: hashInfo.hash}).then(() => moderatorPanelStore.reset())}>Accept</Button>
         </div>
     </div>
