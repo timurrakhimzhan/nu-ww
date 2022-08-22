a0355d4e7ceb11c8eaee9993a079c8668db1c458
Timur Rakhimzhan <t.rakhimzhan@eyelab.kz>
2022-08-23 02:28:02 +0600

    Clublist import changed

--- components/event-info/index.tsx
+++ components/event-info/index.tsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 
 import styles from './EventInfo.module.scss';
 import EventsTable from "../events-table";
-import dynamic from "next/dynamic";
+import ClubsList from "../clubs-list";
 
 export type EventInfoProps = {
     heading: string;
@@ -17,17 +17,12 @@ export type EventInfoProps = {
     clubList?: Array<string>
 }
 
-const ClientClubList = dynamic(
-    () => import('../clubs-list'),
-    {ssr: false}
-)
-
 const EventInfo: React.FC<EventInfoProps> = ({ heading, subheading, description, eventsSchedule, clubList, isHidden = false}) => {
     return <article aria-hidden={isHidden} className={styles.wrapper}>
         <h1 className={styles.heading}>{heading}</h1>
         <h4 className={styles.subheading}>{subheading}</h4>
         <p className={styles.description}>{description}</p>
-        {clubList &&  <div className={styles.clubWrapper}><ClientClubList clubs={clubList}/></div>}
+        {clubList &&  <div className={styles.clubWrapper}><ClubsList clubs={clubList}/></div>}
         {eventsSchedule && <div className={styles.tableWrapper}><EventsTable items={eventsSchedule}/></div>}
     </article>
 }
